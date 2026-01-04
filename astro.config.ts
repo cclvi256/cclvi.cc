e4456d723d47b9570c16ce6d40402b3974cae44f
cc <i@cclvi.cc>
2026-01-04 21:11:16 +0800

    fix: Switch vercel adaptor to cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
+import cloudflare from '@astrojs/cloudflare';
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig, fontProviders, svgoOptimizer } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
@@ -40,7 +40,7 @@ export default defineConfig({
 
   // [Adapter]
   // https://docs.astro.build/en/guides/deploy/
-  adapter: vercel({ imageService: true }),
+  adapter: cloudflare(),
   output: 'server',
   // Local (standalone)
   // adapter: node({ mode: 'standalone' }),
